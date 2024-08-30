1611bcb07f041817299d89843226bacd1b80d5ff
yasuo-nakagawa <nakagawa@yasuo.official.jp>
2024-08-30 16:23:01 +0900

    240830

---
+++ test/destroy.php
@@ -0,0 +1,20 @@
+<?php
+session_start();
+unset($_SESSION['count']);
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<p><a href="#" onClick="history.back(); return false;">前のページにもどる</a></p>
+</body>
+</html>
+
+
+
--- test/test.php
+++ test/test.php
@@ -1,12 +1,17 @@
 <?php
 session_start();
+//セッションIDを推測されてしまう値に設定しない
+//echo session_id();
+session_regenerate_id(true); // 新しい、セッションIDを生成する
+echo session_id(); // 上のセッションIDとは違うIDが生成されます
+
 if(isset($_SESSION['count'])) {
   $count = $_SESSION['count'];
 } else {
   $count = 0;
 }
  
-echo $count;
+echo "更新回数" . $count . "回"; 
 $count++;
 $_SESSION['count'] = $count;
 
@@ -22,12 +27,22 @@ try {
   $db  = new PDO('mysql:host=' . HOSTNAME . ';dbname=' . DATABASE, USERNAME, PASSWORD);
   $msg = "MySQL への接続確認が取れました。";
 
+  //SQLインジェクション↓ダメな例
+  // $sql = 'select * from test';
+  // foreach ($db->query($sql) as $row) {
+  //     print($row['test_name']);
+  //     print($row['test_add'].'<br>');
+  // }
 
-  $sql = 'select * from test';
-  foreach ($db->query($sql) as $row) {
-      print($row['test_name']);
-      print($row['test_add'].'<br>');
-  }
+  //SQLインジェクション対策(プリペアドステートメント)
+  $stmt = $db->prepare('SELECT * FROM test WHERE test_id = :test_id');
+  $stmt->bindValue(':test_id', 1, PDO::PARAM_INT);
+  $stmt->execute();
+  $data = $stmt->fetchAll();
+  foreach ($data as $row) {
+    print($row['test_name']);
+    print($row['test_add'].'<br>');
+}
 
 
 
@@ -40,10 +55,10 @@ try {
 <html>
   <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-    <title>MySQL接続確認</title>
+    <title>色々テスト</title>
   </head>
   <body>
-    <h1>MySQL接続確認</h1>
     <p><?php echo $msg; ?></p>
+    <a href="destroy.php">セッション破棄する</a>
   </body>
 </html>
